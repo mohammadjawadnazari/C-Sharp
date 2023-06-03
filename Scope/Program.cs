d249951bbe9731a5c38b171d7fe3c47da66935ce
M Jawad <129998566+mohammadjawadnazari@users.noreply.github.com>
2023-06-03 05:36:06 +0430

    Add files via upload

---
+++ Scope/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10;
+            for (int i = 0; i<5; i++)
+            {
+                int j = 90;
+                Console.Write(i + "\t" );
+                
+                Console.Write(j + "\t" );
+                
+                Console.WriteLine(a + "\t");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                int j = 90;
+                Console.Write(i + "\t");
+
+                Console.Write(j + "\t");
+
+                Console.WriteLine(a + "\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}

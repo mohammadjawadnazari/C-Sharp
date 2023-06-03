2890cd1692dd1a8c1beccb21090837580a66849c
M Jawad <129998566+mohammadjawadnazari@users.noreply.github.com>
2023-06-03 05:17:10 +0430

    Add files via upload

---
+++ 5th Week/Constant and Letrals/Constant and Letrals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant_and_Letrals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double Pi = 3.14;
+            Console.WriteLine("\n \t Pi = {0} \n ", Pi);
+
+            Console.WriteLine("\n \t Pi = {0} \n ", Pi);
+
+            Console.WriteLine("\n \t \v Pi = {0} \n ", Pi);
+
+            Console.WriteLine("\n \t \b\'Pi\' = {0} \n ", Pi);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5th Week/Conversion/Conversion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int A, B;
+            //double C;
+            //A = 2;
+            //B = 3;
+
+            //C = (double)A / B;
+            double A = 6.79;
+            int B;
+            B = Convert.ToInt32(A);
+            Console.WriteLine("B = {0}", B);
+            Console.WriteLine("_________________*************_______________");
+
+            B = 0;
+            B = (int)A;
+            Console.WriteLine("B = {0}", B);
+
+            //B = 0;
+            //B = int.Parse(A);
+            //Console.WriteLine("B = {0}", B);
+
+            Console.ReadLine();
+
+        }
+    }
+}

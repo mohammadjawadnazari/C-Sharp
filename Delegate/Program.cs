f4d1e999ba10cb32d53b07ada7591d6469ae62f4
M Jawad <129998566+mohammadjawadnazari@users.noreply.github.com>
2023-06-03 05:34:17 +0430

    Add files via upload

---
+++ Delegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+
+        static public int sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static public int sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        static public int ul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        static public int div(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+
+        public delegate int worker(int x, int y);
+        static void Main(string[] args)
+        {
+            int w = 90, z = 100;
+            worker Mathman = new worker(Sum);
+            Mathman += sum;
+            Mathman -= sub;
+            Mathman *= mul;
+            Mathman /= div;
+        }
+    }
+}

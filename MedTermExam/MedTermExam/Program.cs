a18794a82a04f1888a8db3555dc0b5df7b1c08ff
M Jawad <129998566+mohammadjawadnazari@users.noreply.github.com>
2023-06-03 05:41:57 +0430

    Add files via upload

---
+++ MedTermExam/MedTermExam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedTermExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("thank you very much for letting me to speak,");
+            Console.WriteLine("I want to talk about KNOWLEDGE.");
+            Console.ReadLine();
+
+        }
+    }
+}

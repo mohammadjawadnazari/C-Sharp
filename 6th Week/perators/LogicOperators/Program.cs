f9b1b57894b48e14882e6bbb88e06db133260cb7
M Jawad <129998566+mohammadjawadnazari@users.noreply.github.com>
2023-06-03 05:21:21 +0430

    Add files via upload

---
+++ 6th Week/perators/LogicOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a;
+            a = Convert.ToBoolean(0.4);
+            Console.WriteLine(a);
+            //int Age;
+            //Console.WriteLine("Enter your Age");
+            //Age = Convert.ToInt32(Console.ReadLine());
+            //bool ans = (
+            //Console.WriteLine("You are "+ ((Age < 30) && (Age > 20))+ " young");
+            //Console.WriteLine("you are " + ans + "Young");
+            //Console.WriteLine("You are " + ((Age > 30) && (Age < 50)) + " medal man");
+            //Console.WriteLine("You are " + ((Age == 30) || (Age <= 20) && (Age >=50)) + " commnt");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 6th Week/perators/NameSpace/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using NameSpace2;
+
+namespace NameSpace1
+{
+    class C1
+    {
+        public void F1()
+        {
+            Console.WriteLine("Your in NameSpace \"F1\" and class \"C1\" and function \"F1\"");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C1 obj1 = new C1();
+            NameSpace1.C1 obj1 = new NameSpace1.C1();
+            NameSpace2.C1 obj2 = new NameSpace2.C1();
+            obj1.F1();
+            obj2.F1();
+
+            Console.ReadLine();
+            //string ans;
+            //Console.WriteLine("Enter your ans");
+            //ans = Console.ReadLine();
+            //A.print(ans);
+            //Console.ReadLine();
+        }
+    }
+}
+namespace NameSpace2
+{
+    class C1
+    {
+        public void F1()
+        {
+            Console.WriteLine("Your in NameSpace \"F2\" and class \"C1\" and function \"F1\"");
+        }
+    }
+    //class A
+    //{
+    //    static public void print(string x)
+    //    {
+    //        Console.WriteLine(x);
+    //    }
+    //}
+}

a57fcab3896d29c585ed4afd31bcb2e56b3b7ad3
M Jawad <129998566+mohammadjawadnazari@users.noreply.github.com>
2023-06-03 05:35:10 +0430

    Add files via upload

---
+++ Main Fun/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Fun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string x in args)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
